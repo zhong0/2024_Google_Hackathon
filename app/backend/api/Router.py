757b1e5b7f2425aeaf34f451bdb452b720d95d8b
WeiHaoWang <fred870107@gmail.com>
2024-04-23 01:12:30 +0800

    refactor: rename API tag

--- app/backend/api/Router.py
+++ app/backend/api/Router.py
@@ -4,6 +4,6 @@ from .RecommendApi import router as recommend_router
 from .AuthApi import router as athu_router
 
 api_router = APIRouter()
-api_router.include_router(clothes_router, prefix="/clothes", tags=["clothes"])
-api_router.include_router(recommend_router, prefix="/recommend", tags=["recommend"])
-api_router.include_router(athu_router, prefix="/athu", tags=["athu"])
+api_router.include_router(clothes_router, prefix="/clothes", tags=["Clothes"])
+api_router.include_router(recommend_router, prefix="/recommend", tags=["Recommend"])
+api_router.include_router(athu_router, prefix="/athu", tags=["Authorization"])
