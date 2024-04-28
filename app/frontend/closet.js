ca3e4c51a84c0a9b22d68e69845a6869506552cd
zhong0 <zhong0413@gmail.com>
2024-04-29 00:46:51 +0900

    feat: closet combination of frontend and backend

--- app/frontend/closet.js
+++ app/frontend/closet.js
@@ -3,29 +3,95 @@ const closet_container = document.getElementById('closet_container');
 const choice_list = document.getElementById('choice-list');
 const delete_button =  document.getElementById('delete-bt');
 const addStore_button =  document.getElementById('add-to-store-bt');
-const clothesDetail_input = document.getElementById('add-store-detail');
+const clothesDetail_container = document.getElementById('add-store-detail');
 const favorite_button =  document.getElementById('favorite-bt');
 const store_button =  document.getElementById('store-bt');
+const username_text =  document.getElementById('username-text');
+const price_input = document.getElementById('price-input');
+const size_input = document.getElementById('size-input');
+const brand_input = document.getElementById('brand-input');
+const details_input = document.getElementById('detail-input');
+const sell_button = document.getElementById('add-to-sell');
+
+const username = 'zhong0'; //localStorage.getItem('username');
+const loading_container = document.getElementById('loading');
+
 
 let selectedImage = null;
 let previousSelected = null;
+let options = [];
+let imageListData = [];
+username_text.textContent = username;
 
-let options = ["Top", "Bottom", "Shoes", "Clothes"];
-let data = [
-    { id: 1, filename: "../upload/zhong0/01.jpg", 'category': 'Bottom'},
-    { id: 2, filename: "../upload/zhong0/02.jpg", 'category': 'Bottom'},
-    { id: 3, filename: "../upload/zhong0/03.jpg", 'category': 'Bottom'},
-    { id: 4, filename: "../upload/zhong0/04.jpg", 'category': 'Top'},
-    { id: 5, filename: "../upload/zhong0/05.jpg", 'category': 'Bottom'}
-];
+function createInitOptions() {
+    const option = document.createElement('option');
+    option.value = '';
+    option.text = 'Please select an option';
+    option.selected = true;
 
+    clothestype_dropdown.appendChild(option);
+}
+
+function getAllClothes() {
+    loading_container.style.display = 'flex';
+    const form_data = new FormData();
+    form_data.append('username',username);
+
+    const request_options = {
+        method:'POST',
+        body:form_data
+    }
+    
+    fetch('/clothes/file-path-group-by-category', request_options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let idex = 1;
+            let file_path = '../upload/';
+
+            // set init
+            imageListData = [];
+            options = [];
+            clothestype_dropdown.innerHTML = ''; 
+            closet_container.innerHTML = "";
+            createInitOptions();
+
+            // set imageListData
+            for(let category in data.file_path){
+                options.push(category);
+                let filenames = data.file_path[category];
+                filenames.forEach( (filename) =>{
+                    imageListData.push({ id: idex, filename: file_path+filename, chosen: false, category: category});
+                    idex += 1;
+                });
+            }
+            clotheChosen(imageListData);
+            //set dropdown by options
+            options.forEach(option => {
+                const optionElement = document.createElement("option");
+                optionElement.text = option;
+                optionElement.value = option;
+                clothestype_dropdown.appendChild(optionElement);
+            });
+            loading_container.style.display = 'none';
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+// choose a clothe
 function clotheChosen(imageList) {
     imageList.forEach((ele) => {
         const imageWrapper = document.createElement('div');
         imageWrapper.classList.add('image-wrapper');
         // 創建 img 元素
         const img = document.createElement('img');
-        img.src = `../upload/${ele.filename}`; // 圖片路徑根據索引 i 設置
+        img.src = ele.filename; // 圖片路徑根據索引 i 設置
         img.id = ele.id;
         img.alt = 'Image ' + ele.id;
     
@@ -45,7 +111,7 @@ function clotheChosen(imageList) {
                     image.classList.remove('hovered');
                 });
                 choice_list.style.display = 'none';
-                clothesDetail_input.style.display = 'none';
+                clothesDetail_container.style.display = 'none';
             }
             if (img === selectedImage) {
                 selectedImage = null;
@@ -57,56 +123,119 @@ function clotheChosen(imageList) {
             }
         });
     
-        // 將 input、label 和 img 元素添加到 imageWrapper 中
         imageWrapper.appendChild(img);
-        
-        // 將 imageWrapper 添加到父容器中
         closet_container.appendChild(imageWrapper);
     })
 
 }
 
-clotheChosen(data);
-
-options.forEach(option => {
-    const optionElement = document.createElement("option");
-    optionElement.text = option;
-    optionElement.value = option;
-    clothestype_dropdown.appendChild(optionElement);
-});
-
+// dropdown change
 clothestype_dropdown.addEventListener("change", function() {
     selectedValue = clothestype_dropdown.value;
     choice_list.style.display = 'none';
-    clothesDetail_input.style.display = 'none';
+    clothesDetail_container.style.display = 'none';
     closet_container.innerHTML = "";
-    let filterImage = []
+    let filterImage = [];
     if (selectedValue !== "") {
-        filterImage = data.filter((ele) => (ele.category === selectedValue));
+        filterImage = imageListData.filter((ele) => (ele.category === selectedValue));
     } else {
-        filterImage = data.filter((ele) => (!chosenList.includes(ele.id)));
+        filterImage = imageListData;
     }
     clotheChosen(filterImage)
     
 });
 
+// remove from closet
 delete_button.addEventListener('click', () => {
+    const filename = `${username}/${selectedImage.src.split("/").slice(-1)[0]}`;
+    const form_data = new FormData();
+    
+    form_data.append('username',username);
+    form_data.append('filename',filename);
+
+    const request_options = {
+        method:'POST',
+        body:form_data
+    }
+    
+    fetch('/clothes/remove-clothes-from-wardrobe', request_options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            choice_list.style.display = 'none';
+            clothesDetail_container.style.display = 'none';
+            getAllClothes();
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
     
 });
 
+// add-to-store button
 addStore_button.addEventListener('click', () => {
-    if(clothesDetail_input.style.display === 'none') {
-        clothesDetail_input.style.display = 'block';
+    if(clothesDetail_container.style.display === 'none') {
+        clothesDetail_container.style.display = 'block';
     } else {
-        clothesDetail_input.style.display === 'none'
+        clothesDetail_container.style.display === 'none'
     }
     
 });
 
-clothesDetail_input.addEventListener('click', () => {
-    
+// sell
+sell_button.addEventListener('click', () => {
+    const price =  parseFloat(price_input.value);
+    const size = size_input.value;
+    const brand = brand_input.value;
+    const details = details_input.value;
+
+    const payload_data = {
+        username: username,
+        filename: `${username}/${selectedImage.src.split("/").slice(-1)[0]}`,
+        size: size,
+        brand: brand,
+        owner_description: details,
+        price: price,
+    }
+
+    const request_options = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload_data)
+    }
+
+    fetch('/shop/clothes-on-sale', request_options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            choice_list.style.display = 'none';
+            clothesDetail_container.style.display = 'none';
+            getAllClothes();
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+
 });
 
+
+// page init - get all clothes
+document.addEventListener('DOMContentLoaded', function() {
+    getAllClothes();
+});
+
+
+// go to favorite set page
 favorite_button.addEventListener('click', () => {
     fetch('/favorite_set', { method: 'GET' })
     .then(response => {
@@ -121,6 +250,7 @@ favorite_button.addEventListener('click', () => {
     });
 });
 
+// go to store page
 store_button.addEventListener('click', () => {
     fetch('/store', { method: 'GET' })
     .then(response => {
